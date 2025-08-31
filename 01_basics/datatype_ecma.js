87f0d9d0bf54face4469faa2f8199832bea87fa2
Shrutikadubey <113886718+Shrutikadubey@users.noreply.github.com>
2025-08-31 08:44:54 +0000

    datatype

---
+++ 01_basics/datatype_ecma.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all Js code as newer version
+
+// alert("hello") //we are using node.js ,not browser
+
+console.log(3+
+    3)//code readility should be high
+
+    // ecma script and mdn  is documentaion of js
+let name="yasha"
+let age=13
+let isLoggedIn=false
+
+
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standard alone value
+//undefined=>
+//symbol=> used 
---
+++ 01_basics/variable.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail="hitesh@google.com"
+var password="1234"
+accountCity='raipur'
+
+
+// accountId=267 //not allowed to change the value of const
+
+accountEmail="hitesh@google.com"
+password="14"
+accountCity='raipur'
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountState])
+
+/*prefer not to use var
+bcoz of issue in block and functional scope*/ 
