76be32b99265c7063dc60fe3658b4d486a20972d
Shrutikadubey <113886718+Shrutikadubey@users.noreply.github.com>
2025-08-31 17:11:06 +0000

    conversion of datatype

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,25 @@
+let score=undefined
+
+console.log(typeof(score));
+
+let valueIn_no=Number(score)
+console.log(typeof(valueIn_no));
+
+//"33"=>33
+//"33abs"=>NaN
+//true=>1; false=>0
+
+let isLoggedIn=1
+let booleanisloggedin=Boolean(isLoggedIn)
+console.log(booleanisloggedin);
+
+
+//1=>true;  0=>false
+//""=>false
+//"hitesh"=>true
+
+let someno=33
+let strno=String(someno)
+console.log(strno);
+console.log(typeof(strno));
+
